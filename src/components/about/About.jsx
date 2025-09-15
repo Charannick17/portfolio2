80f194d78331ae9a1820465e36a2cb973eafb72e
Charannick17 <95394887+Charannick17@users.noreply.github.com>
2025-09-15 11:10:47 +0000

    final_

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./about.css";
-import AboutImg from "../../Assets/my_image__.jpg";
+import AboutImg from "../../Assets/myImage.jpg";
 import CV from "../../Assets/Nese_CharanTeja_resume_.pdf";
 import Info from './info';
 // import ScrollDown from '..//about/ScrollDown';
