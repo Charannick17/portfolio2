93d41b09ad13044e5323b70010c64ca508de00e9
Charannick17 <95394887+Charannick17@users.noreply.github.com>
2025-09-15 06:54:59 +0000

    footer__updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,15 +5,15 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <p className="footer__text">© {new Date().getFullYear()} Maile Venu Madhav. All rights reserved.</p>
+        <p className="footer__text"> </p>
         <div className="footer__socials">
-          <a href="https://github.com/Venu-16" target="_blank" rel="noopener noreferrer" className="footer__link">
+          <a href="https://github.com/Charannick17" target="_blank" rel="noopener noreferrer" className="footer__link">
             <i className='bx bxl-github'></i>
           </a>
-          <a href="https://linkedin.com/in/venu-madhav-maile-16v" target="_blank" rel="noopener noreferrer" className="footer__link">
+          <a href="https://www.linkedin.com/in/nese-charan-teja-56145b27a/" target="_blank" rel="noopener noreferrer" className="footer__link">
             <i className='bx bxl-linkedin'></i>
           </a>
-          <a href="mailto:venumadhav2216@gmail.com" className="footer__link">
+          <a href="mailto:ncharan229@gmail.com" className="footer__link">
             <i className='bx bx-envelope'></i>
           </a>
         </div>
