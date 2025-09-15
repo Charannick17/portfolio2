32c732823f30a3bd61065d014eb216b6a1683daf
Charannick17 <95394887+Charannick17@users.noreply.github.com>
2025-09-15 10:29:16 +0000

    coding_leetcode_

--- src/components/Coding Profiles/profiles.jsx
+++ src/components/Coding Profiles/profiles.jsx
@@ -5,7 +5,7 @@ const CodingProfiles = () => {
   const profiles = [
     {
       name: "LeetCode",
-      link: "https://x.com/Charannick17",
+      link: "https://leetcode.com/u/charannick/",
       logo: "https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png"
     },
     {
